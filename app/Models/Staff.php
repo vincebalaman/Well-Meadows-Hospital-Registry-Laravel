c0e59189a241ad26cc441586131eef46cbd5027e
Ken -J <vanzuela.kenjan33@gmail.com>
2026-05-10 07:49:09 +0800

    added contract and allocation

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\StaffAllocation;
+use App\Models\StaffContract;
 use Illuminate\Database\Eloquent\Model;
 
 class Staff extends Model
@@ -31,4 +33,14 @@ class Staff extends Model
     protected $casts = [
         'dob' => 'date',
     ];
+
+    public function contract()
+    {
+        return $this->hasOne(StaffContract::class, 'staff_no', 'staff_no');
+    }
+
+    public function allocations()
+    {
+        return $this->hasMany(StaffAllocation::class, 'staff_no', 'staff_no');
+    }
 }
